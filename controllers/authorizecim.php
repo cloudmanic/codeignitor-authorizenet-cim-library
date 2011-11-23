bf3b395d351f9e4595f662c10c6700696ce982cb
Cloudmanic Labs, LLC <github@cloudmanic.com>
2011-11-23 12:57:17 -0800

    Upgraded controller to CI 2.0

--- controllers/authorizecim.php
+++ controllers/authorizecim.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class AuthorizeCim extends Controller
+class AuthorizeCim extends CI_Controller
 {	
 	public $data = array();
 	
